bac5bda9b71666c584e1f697d6fb48cc6be0b150
Bioinformatic Research Group in Epidemiology (BRGE) <juanr.gonzalez@isglobal.org>
2019-05-24 19:43:39 +0200

    ...

--- R/plot_chromatine_states.R
+++ R/plot_chromatine_states.R
@@ -1,25 +1,86 @@
-library(RCurl)
-x <- getURL("https://raw.githubusercontent.com/isglobal-brge/brgeUtils/master/data/chrom_states_descr.txt")
-chrom_states <- read.delim(text=x, as.is = TRUE)
-
-df <- data.frame(state=rep(chrom_states$State,2),
-                 pvalue=c(runif(15, 0, 1),runif(15, 0, 1)),
-                 beta=rep(c(-1,-1,-1, -1, rep(1, 11)),2),
-                 expos = rep(c("A", "B"), each=15))
-
-
-df2 <- merge(df, chrom_states, by.x="state", by.y="State")
-
-df2$State <- factor(df2$state, 
-                    levels=rev(chrom_states$State[chrom_states$Order]))
-
 library(ggplot2)
-df2$state2 <- paste0(df2$Description, " (", df2$state, ")")
-ggplot(df2, aes(expos, state2, col=beta)) +
-  geom_point(aes(size = pvalue)) + 
-  theme(axis.text.y = 
-          element_text(colour = 
-          rev(col2hex(chrom_states$color)[chrom_states$Order])))
+library(grid)
+library(RCurl)
+ff <- getURL("https://raw.githubusercontent.com/isglobal-brge/brgeUtils/master/data/chrom_states_descr.txt")
+chrom_states <- read.delim(text=ff, as.is = TRUE)
 
-df2$color
-colors()
+element_custom <- function(...) {
+  structure(list(...), class = c("element_custom", "element_blank"))
+}
+
+element_grob.element_custom <- function(element, label, x, y, ...)  {
+  tg <- textGrob(label, y=y, gp=gpar(col=element$colour))
+  padding <- unit(1,"line")
+  rg <- rectGrob(y=y,width=grobWidth(tg)+padding, 
+                 height=unit(1,"line")+padding, 
+                 gp=gpar(fill = element$fill, col=NA, alpha=1))
+  gTree(children=gList(rg, tg), width=grobWidth(tg) + padding, cl="custom_axis")
+}
+
+widthDetails.custom_axis <- function(x) x$width + unit(1,"mm") # fudge
+
+
+
+
+
+plotChromStates <- function(x, chrom_states, state.x="state", 
+                            tile=TRUE, tit){
+  df <- merge(chrom_states, x, by.x="State", by.y=state.x)
+  levels(df$beta) <- c("OR<1", "OR>1")
+  df$values <- NA
+  df$values[df$beta=="OR<1" & df$p.value<0.05/15] <- 1
+  df$values[df$beta=="OR>1" & df$p.value<0.05/15] <- 2
+  df$values <- as.factor(df$values)
+  
+  df$state2 <- paste0(df$Description, " (",
+                      df$State, ")")
+  chrom_states$state2 <- paste0(chrom_states$Description, " (",
+                                chrom_states$State, ")")
+  ll <- rev(chrom_states$state2[chrom_states$Order])
+  df$State <- factor(df$state2, levels=ll)
+  
+  col2hex <- function(col, alpha) rgb(t(col2rgb(col)), 
+                                      alpha=alpha, 
+                                      maxColorValue=255)
+  
+  mycol <- rev(col2hex(chrom_states$color)[chrom_states$Order])
+  
+  if (!tile)
+    ggplot(df, aes(lab.exposure, State, size = -log10(p.value))) +
+    geom_point(aes(col=values)) + 
+    ylab("Blood chromatin states (ROADMAP)") +
+    xlab("Exposures") + 
+    scale_colour_manual(values=c("red", "blue"),
+                        labels = c("OR<1", "OR>1"),
+                        na.translate = FALSE) +
+    scale_size_continuous(breaks=c(2,3,4),
+                          labels=c(2,3,4)) +
+    labs(colour = "Enrichment") + 
+    theme(axis.text.y = element_custom(fill=mycol),
+          axis.title.y = element_text(size = 16, face = "bold"),
+          axis.text.x = element_text(angle = 90, hjust=1,
+                                     vjust = 0.5)) +
+    ggtitle(tit)
+  else
+    ggplot(df, aes(lab.exposure, State)) +
+    geom_raster(aes(fill = values), hjust=0) +
+    ylab("Blood chromatin states (ROADMAP)") +
+    xlab("Exposures") +
+    scale_fill_manual(values = c('red','blue'),
+                      labels = c("OR<1", "OR>1"),
+                      na.translate = FALSE) +
+    labs(fill = "Enrichment") + 
+    theme(axis.text.y = element_custom(fill=mycol),
+          axis.title.y = element_text(size = 16, face = "bold"),
+          axis.text.x = element_text(angle = 90, hjust=1),
+          axis.ticks = element_blank()) +
+    ggtitle(tit)
+}
+
+
+
+
+
+
+
+
